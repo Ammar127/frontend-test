d7d693bc59e488a99ca55d7d3b5c4e495fb61a32
Ammar Iqbal <ammariqbal127@gmail.com>
2020-09-23 12:58:22 +0500

    bootstrap at index

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import Bottom from '../components/Bottom'
+import Featured from '../components/Featured'
+import Header from '../components/Header'
+import List from '../components/List'
+import Slider from '../components/Slider'
 
 export default class Home extends Component {
     render() {
         return (
-            <div>
-                <p>hello</p>
-            </div>
+            <>
+                <Header/>
+                <Featured/>
+                <List/>
+                <Slider/>
+                <Bottom/>
+            </>
         )
     }
 }
